f2ee973d314fa747439758995cf094be48eb0a30
Raphael Grimm <raphael.grimm@kit.edu>
2018-01-25 16:22:57 +0100

    Change doc comments to doxygen comments

--- src/Polyhedron.h
+++ src/Polyhedron.h
@@ -26,23 +26,22 @@
 #include <utility>
 
 namespace Eigen {
-
-/* Wrapper of Convex Polyhedron
+/** Wrapper of Convex Polyhedron
  * This class aims to translate eigen matrix into cddlib matrix.
  * It automatically transforms a v-polyhedron into an h-polyhedron and vice-versa.
  */
 class Polyhedron {
 public:
-    /* Free the pointers and unset the cdd global constants. */
+    /** Free the pointers.*/
     ~Polyhedron();
 
-    /* Treat the inputs as a H-representation and compute its V-representation.
+    /** Treat the inputs as a H-representation and compute its V-representation.
      * H-polyhedron is such that \f$ Ax \leq b \f$.
      * \param A The matrix part of the representation of the polyhedron.
      * \param b The vector part of the representation of the polyhedron.
      */
     void vrep(const Eigen::MatrixXd& A, const Eigen::VectorXd& b);
-    /* Treat the inputs as a V-representation and compute its H-representation
+    /** Treat the inputs as a V-representation and compute its H-representation
      * V-polyhedron is such that \f$ A = [v r]^T, b=[1^T 0^T]^T \f$
      * with A composed of \f$ v \f$, the vertices, \f$ r \f$, the rays
      * and b is a vector which is 1 for vertices and 0 for rays.
@@ -50,22 +49,22 @@ public:
      * \param b The vector part of the representation of the polyhedron.
      */
     void hrep(const Eigen::MatrixXd& A, const Eigen::VectorXd& b);
-    /* Get the V-representation of the polyhedron
+    /** Get the V-representation of the polyhedron
      * V-polyhedron is such that \f$ A = [v^T r^T]^T, b=[1^T 0^T]^T \f$
      * with A composed of \f$ v \f$, the vertices, \f$ r \f$, the rays
      * and b is a vector which is 1 for vertices and 0 for rays.
      * \return Pair of vertices and rays matrix and identification vector of vertices and rays for the V-representation
      */
     std::pair<Eigen::MatrixXd, Eigen::VectorXd> vrep() const;
-    /* Get the H-representation of the polyhedron
+    /** Get the H-representation of the polyhedron
      * H-polyhedron is such that \f$ Ax \leq b \f$.
      * \return Pair of inequality matrix and inequality vector for the H-representation
      */
     std::pair<Eigen::MatrixXd, Eigen::VectorXd> hrep() const;
 
-    /* Print the H-representation of the polyhedron */
+    /** Print the H-representation of the polyhedron */
     void printHrep() const;
-    /* Print the V-representation of the polyhedron */
+    /** Print the V-representation of the polyhedron */
     void printVrep() const;
 
 private:
